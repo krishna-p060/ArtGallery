f97c252ff3230b6b4b43ff8b07f6c1ab51ad5032
Krish Patidar <krishnarp060@gmail.com>
2025-11-25 01:02:23 +0800

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Krishna Patidar on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
